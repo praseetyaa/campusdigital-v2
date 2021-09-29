ce47b41d708a61500b5d3f6ddbd05391c18d965a
isnaprasetyo <praseetyaa@gmail.com>
2021-09-29 16:05:38 +0700

    update hourly

--- resources/views/page/tentang-kami.blade.php
+++ resources/views/page/tentang-kami.blade.php
@@ -4,74 +4,132 @@
 
 @section('content')
 
-<section class="page-top-section">
+<section class="page-top-section pb-0">
   <div class="container">
-    <div class="row align-items-center header-one">
+    <div class="header-one">
       {{-- <div class="col-lg-6 order-2 order-lg-1 text-center text-lg-start"> --}}
-      <div class="text-center border-bottom"><img src="{{ asset('assets/images/logo/'.setting('site.logo')) }}" alt="logo-campusdigital" class="logoCdProfile"></div>
-      <hr>
-      <div class="col-12">
-        <h1>Tentang Kami</h1>
-        <p>&emsp;&emsp;Era Digital memberi pengaruh terhadap perkembangan teknologi yang sangat cepat. Perkembangan teknologi mendorong setiap pribadi untuk selalu siap berubah dan bertumbuh. Persaingan dunia usaha dan dunia industri di era digital juga semakin ketat. Hadirnya persaingan yang ketat di dunia usaha dan industri menuntut dukungan sumber daya manusia yang professional dan kompeten. Kebutuhan akan SDM yang professional dan kompeten melahirkan  Campus Digital untuk menyajikan program pendidikan dan pelatihan dalam rangka peningkatan kompetensi sumber daya manusia yang unggul.</p>
-        <p>&emsp;&emsp;Campus Digital merupakan Lembaga Pendidikan dan Pelatihan di bidang bisnis dan teknologi digital. Campus Digital adalah salah satu unit bisnis pada PT Campus Data Media, sebuah perusahaan yang bergerak di bidang Jasa Teknologi Informasi yang beraktifitas sejak tahun 2001.</p>
-        <p>&emsp;&emsp;Selaras dengan program pemerintah dalam upaya mempersiapkan SDM Unggul. Campus Digital hadir untuk turut serta memberikan pilihan terbaik bagi masyarakat agar bisa mendapatkan akses pendidikan dan pelatihan yang sesuai dengan kebutuhan Industri, Dunia Usaha dan Kerja (IDUKA).</p>
-        <p>&emsp;&emsp;Campus Digital bekerjasama dengan instansi pemerintah maupun swasta serta beberapa Industri, Dunia Usaha dan Kerja (IDUKA) dan sekolah. Hal ini terus diupayakan agar sistem pembelajaran terjadi link and Match antara lembaga pendidikan dengan Industri, Dunia Usaha dan Kerja (IDUKA). Dengan begitu, akan terbentuk SDM-SDM Unggul yang mempunyai kompetensi sesuai dengan kebutuhan Industri, Dunia Usaha dan Kerja (IDUKA).</p>
+      <div class="section_about mb-4 row align-items-center">
+        <div class="col-12 mb-3">
+          <div class="text-center"><img src="{{ asset('assets/images/logo/'.setting('site.logo')) }}" alt="logo-campusdigital" class="logoCdProfile"></div>
+        </div>
+        <div class="col-12">
+          <p>&emsp;&emsp;Era Digital memberi pengaruh terhadap perkembangan teknologi yang sangat cepat. Perkembangan teknologi mendorong setiap pribadi untuk selalu siap berubah dan bertumbuh. Persaingan dunia usaha dan dunia industri di era digital juga semakin ketat. Hadirnya persaingan yang ketat di dunia usaha dan industri menuntut dukungan sumber daya manusia yang professional dan kompeten. Kebutuhan akan SDM yang professional dan kompeten melahirkan  Campus Digital untuk menyajikan program pendidikan dan pelatihan dalam rangka peningkatan kompetensi sumber daya manusia yang unggul.</p>
+          <p>&emsp;&emsp;Campus Digital merupakan Lembaga Pendidikan dan Pelatihan di bidang bisnis dan teknologi digital. Campus Digital adalah salah satu unit bisnis pada PT Campus Data Media, sebuah perusahaan yang bergerak di bidang Jasa Teknologi Informasi yang beraktifitas sejak tahun 2001.</p>
+          <p>&emsp;&emsp;Selaras dengan program pemerintah dalam upaya mempersiapkan SDM Unggul. Campus Digital hadir untuk turut serta memberikan pilihan terbaik bagi masyarakat agar bisa mendapatkan akses pendidikan dan pelatihan yang sesuai dengan kebutuhan Industri, Dunia Usaha dan Kerja (IDUKA).</p>
+          <p>&emsp;&emsp;Campus Digital bekerjasama dengan instansi pemerintah maupun swasta serta beberapa Industri, Dunia Usaha dan Kerja (IDUKA) dan sekolah. Hal ini terus diupayakan agar sistem pembelajaran terjadi link and Match antara lembaga pendidikan dengan Industri, Dunia Usaha dan Kerja (IDUKA). Dengan begitu, akan terbentuk SDM-SDM Unggul yang mempunyai kompetensi sesuai dengan kebutuhan Industri, Dunia Usaha dan Kerja (IDUKA).</p>
+        </div>
       </div>  
-      <div class="col-lg-6 order-1 order-lg-2 mb-3 mb-lg-0 text-center d-none">
-        <img class="h-auto mb-3 mb-lg-0 img-header" src="{{asset('assets/images/illustration/min/7747.png')}}" alt="banner">
+      <div class="section_visi mb-5">
+        <div style="background-color: var(--primary-s); border-radius: 1rem 1rem 0 0;" class="py-4 text-center">
+          <h2>Campus Digital</h2>
+          <h5 class="fw-normal">The House Of Profesional Digital Technopreneur</h5>
+        </div>
+        <div class="bg-light p-4" style="border-radius: 0 0 1rem 1rem;">
+          <div class="row">
+            <div class="col-lg-6">
+              <h1>Visi</h1>
+              <p>Menjadi perusahaan yang unggul dan terpercaya dalam pelayanan jasa Pendidikan dan Pelatihan di bidang Teknologi Digital dan Bisnis</p>
+            </div>
+            <div class="col-lg-6">
+              <h1>Misi</h1>
+              <ul>
+                <li>Menyediakan layanan program Pendidikan dan pelatihan yang berkualitas</li>
+                <li>Membangun karakter sumber daya manusia yang kompeten dan profesional</li>
+                <li>Membangun tim yang solid, unggul, dan amanah</li>
+                <li>Menyajikan solusi bagi semua mitra bisnis untuk meraih cita-cita mulianya</li>
+                <li>Menjadi model pengelolaan bisnis yang menguntungkan dan mensejahterakan</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Visi</h1>
-        <p>Menjadi perusahaan yang unggul dan terpercaya dalam pelayanan jasa Pendidikan dan Pelatihan di bidang Teknologi Digital dan Bisnis</p>
-        <h1>Misi</h1>
-        <ul>
-          <li>Menyediakan layanan program Pendidikan dan pelatihan yang berkualitas</li>
-          <li>Membangun karakter sumber daya manusia yang kompeten dan profesional</li>
-          <li>Membangun tim yang solid, unggul, dan amanah</li>
-          <li>Menyajikan solusi bagi semua mitra bisnis untuk meraih cita-cita mulianya</li>
-          <li>Menjadi model pengelolaan bisnis yang menguntungkan dan mensejahterakan</li>
-        </ul>
+      <div class="section_budaya mb-5">
+          <div class="heading text-center">
+            <h1>Budaya Kerja</h1>
+          </div>
+          <div class="row justify-content-center mt-4">
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/3736/3736600.svg" width="80">
+                <div>
+                  <h5>Commitment (Memegang Teguh Komitmen)</h5>
+                  <p>Memegang teguh komitmen yang telah kami buat., amanah, bertanggung jawab dan saling percaya satu sama lain.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/2786/2786583.svg" width="80">
+                <div>
+                  <h5>Add Value (Menambah Nilai)</h5>
+                  <p>Selalu memberikan nilai tambah bagi pelanggan, bagi mitra bisnis, bagi lingkungan  sekitar dan masyarakat dunia.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5170/5170221.svg" width="80">
+                <div>
+                  <h5>Mutual Respect (Saling Menghormati dan Menghargai)</h5>
+                  <p>Selalu saling menghargai hasil usaha kontribusi pihak lain. Keterbukaan dan kejelasan informasi dan komunikasi sangat penting, selalu saling membuka diri untuk perbaikan kualitas kinerja kami.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5170/5170063.svg" width="80">
+                <div>
+                  <h5>Professional (Menjaga Sikap Profesionalisme)</h5>
+                  <p>Memanfaatkan keahlian untuk pencapaian target-target perusahaan secara efektif, efisien dan optimal, selalu berfikir, bersikap dan bertindak hati-hati.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5156/5156874.svg" width="80">
+                <div>
+                  <h5>Ultimate (Unggul)</h5>
+                  <p>Sadar bahwa untuk menjadi yang terbaik dan memberikan yang terbaik, memutuskan untuk melakukan sesuatu melebihi standard dan rata-rata orang lain.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+              <div class="service-item d-flex align-items-start p-3">
+                <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5156/5156788.svg" width="80">
+                <div>
+                  <h5>Service Excellent ( Pelayanan yang Bermutu)</h5>
+                  <p>Memberikan pelayanan yang bermutu, bekerja dan melayani dengan baik saja tidak cukup, harus bekerja dengan predikat yang luar biasa.</p>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
-      <div>
-        <h1>Budaya Kerja</h1>
-        <ul>
-          <li>Commitment (Memegang Teguh Komitmen)</li>
-          <p>Memegang teguh komitmen yang telah kami buat., amanah, bertanggung jawab dan saling percaya satu sama lain. </p>
-          <li>Add Value (Menambah Nilai)</li>
-          <p>Selalu memberikan nilai tambah bagi pelanggan, bagi mitra bisnis, bagi lingkungan  sekitar dan masyarakat dunia.</p>
-          <li>Mutual Respect (Saling Menghormati dan Menghargai)</li>
-          <p>Selalu saling menghargai hasil usaha kontribusi pihak lain. Keterbukaan dan kejelasan informasi dan komunikasi sangat penting, selalu saling membuka diri untuk perbaikan kualitas kinerja kami.</p>
-          <li>Professional (Menjaga Sikap Profesionalisme)</li>
-          <p>Memanfaatkan keahlian untuk pencapaian target-target perusahaan secara efektif, efisien dan optimal, selalu berfikir, bersikap dan bertindak hati-hati.</p>
-          <li>Ultimate (Unggul)</li>
-          <p>Sadar bahwa untuk menjadi yang terbaik dan memberikan yang terbaik, memutuskan untuk melakukan sesuatu melebihi standard dan rata-rata orang lain.</p>
-          <li>Service Excellent ( Pelayanan yang Bermutu)</li>
-          <p>Memberikan pelayanan yang bermutu, bekerja dan melayani dengan baik saja tidak cukup, harus bekerja dengan predikat yang luar biasa.</p>
-        </ul>
+      <div class="section_kelebihan mb-5">
+        <div class="heading text-center mb-4">
+          <h1>Kelebihan Campus Digital</h1>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <li>Instruktur yang tersertifikasi</li>
+            <li>Video Pembelajaran yang aplikatif</li>
+            <li>Peluang berkarir di Perusahaan mitra</li>
+            <li>Pengelola dan instruktur tersertifikasi</li>
+            <li>Modul Pembelajaran selalu up to date</li>
+          </div>
+          <div class="col-lg-6">
+            <li>Lokasi yang strategis dan mudah dijangkau</li>
+            <li>Kerjasama dengan instansi pemerintah maupun swasta</li>
+            <li>Peserta berkesempatan mendapatkan sertifikasi kompetensi</li>
+            <li>Waktu belajar yang fleksibel bisa belajar kapan dan dimana saja</li>
+            <li>Program affiliate yang memberi potensi penghasilan langsung  peserta</li>
+            <li>Mentor dan instruktur merupakan praktisi bisnis dan teknologi digital tersertifikasi</li>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Tujuan Sasaran Pendidikan</h1>
-        <p>&emsp;&emsp;Campus Digital melatih semua peserta dan kandidat untuk menjadi kompeten dan professional dengan program pembelajaran praktis, sistematis, dan efektif disesuaikan dengan perkembangan dan kebutuhan Industri, Dunia Usaha dan Kerja (IDUKA).</p>
-        <p>&emsp;&emsp;Materi-materi pembelajaran mengikuti Standar Kompetensi Kerja Nasional Indonesia (SKKNI), sehingga lulusannya merupakan SDM Unggul yang mempunyai kompetensi yang telah tersertifikasi BNSP dan siap kerja maupun berwirausaha.</p>
-      </div>
-      <div>
-        <h1>Kelebihan Campus Digital</h1>
-        <ol>
-          <li>Instruktur yang tersertifikasi</li>
-          <li>Video Pembelajaran yang aplikatif</li>
-          <li>Peluang berkarir di Perusahaan mitra</li>
-          <li>Pengelola dan instruktur tersertifikasi</li>
-          <li>Modul Pembelajaran selalu up to date</li>
-          <li>Lokasi yang strategis dan mudah dijangkau</li>
-          <li>Kerjasama dengan instansi pemerintah maupun swasta</li>
-          <li>Peserta berkesempatan mendapatkan sertifikasi kompetensi</li>
-          <li>Waktu belajar yang fleksibel bisa belajar kapan dan dimana saja</li>
-          <li>Program affiliate yang memberi potensi penghasilan langsung  peserta</li>
-          <li>Mentor dan instruktur merupakan praktisi bisnis dan teknologi digital tersertifikasi</li>
-        </ol>
-      </div>
-      <div>
-        <h1>Fasilitas dan Sarana</h1>
+      <div class="section_fasilitas mb-5">
+        <div class="text-center">
+          <h1>Fasilitas dan Sarana</h1>
+        </div>
         <ol>
           <li>Musholla</li>
           <li>LED Proyektor</li>
@@ -80,27 +138,12 @@
           <li>Materi e-book dan video pembelajaran</li>
         </ol>
       </div>
-      <div>
-        <h1>Cabang Campus Digital</h1>
-        <ol>
-          <li>Campus Digital Gajah Semarang</li>
-          <li>Campus Digital Kedungmundu Semarang</li>
-          <li>Campus Digital Fatmawati Semarang</li>
-          <li>Campus Digital Anjasmoro Semarang</li>
-          <li>Campus Digital Banyumanik Semarang</li>
-        </ol>
-      </div>
-      <div>
-        <h1>Program Kursus dan Pelatihan Campus Digital</h1>
-        <ol>
-          <li>Digital Marketing</li>
-          <li>Graphic Designer</li>
-          <li>Multimedia</li>
-        </ol>
-      </div>
-      <div>
-        <h1>Mengapa Kami?</h1>
-        <p>Dengan memiliki mentor yang ahli di bidangnya, lulusan Campus Digital kami harap akan menjadi tenaga kerja baru di bidang teknologi digital marketing atau menjadi wirausaha yang memiliki marketing online yang handal.</p>
+      <div class="section_why d-flex align-items-center bg-light py-3 px-4 rounded-1">
+        <img src="https://image.flaticon.com/icons/svg/5375/5375522.svg" width="80" class="bg-white rounded-circle p-1 me-3" alt="">
+        <div>
+          <h1>Mengapa Kami?</h1>
+          <p>Dengan memiliki mentor yang ahli di bidangnya, lulusan Campus Digital kami harap akan menjadi tenaga kerja baru di bidang teknologi digital marketing atau menjadi wirausaha yang memiliki marketing online yang handal.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -147,6 +190,7 @@
   .info-text {padding-top: 0;}
   .header-one{margin-bottom: 8rem;}
   .header-one p, .header-one li{text-align: justify;}
+  .section_kelebihan li{margin-bottom: .5rem;}
   @media (max-width: 991.98px) { .header-one{margin-bottom: 4rem;} .spad{padding: 3em 0} }
 </style>
 
