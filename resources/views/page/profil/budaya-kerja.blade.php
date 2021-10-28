09a7b545e61a27a04337c00cb84cc822611370dd
isnaprasetyo <praseetyaa@gmail.com>
2021-10-28 09:41:47 +0700

    bug fix icon

--- resources/views/page/profil/budaya-kerja.blade.php
+++ resources/views/page/profil/budaya-kerja.blade.php
@@ -21,7 +21,7 @@
         <div class="row justify-content-center mt-4">
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/3736/3736600.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/star-rating.png') }}" width="80">
               <div class="text-start">
                 <h5>Commitment (Memegang Teguh Komitmen)</h5>
                 <p>Memegang teguh komitmen yang telah kami buat., amanah, bertanggung jawab dan saling percaya satu sama lain.</p>
@@ -30,7 +30,7 @@
           </div>
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/2786/2786583.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/bar-chart.png') }}" width="80">
               <div class="text-start">
                 <h5>Add Value (Menambah Nilai)</h5>
                 <p>Selalu memberikan nilai tambah bagi pelanggan, bagi mitra bisnis, bagi lingkungan  sekitar dan masyarakat dunia.</p>
@@ -39,7 +39,7 @@
           </div>
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5170/5170221.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/customer.png') }}" width="80">
               <div class="text-start">
                 <h5>Mutual Respect (Saling Menghormati dan Menghargai)</h5>
                 <p>Selalu saling menghargai hasil usaha kontribusi pihak lain. Keterbukaan dan kejelasan informasi dan komunikasi sangat penting, selalu saling membuka diri untuk perbaikan kualitas kinerja kami.</p>
@@ -48,7 +48,7 @@
           </div>
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5170/5170063.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/best-employee.png') }}" width="80">
               <div class="text-start">
                 <h5>Professional (Menjaga Sikap Profesionalisme)</h5>
                 <p>Memanfaatkan keahlian untuk pencapaian target-target perusahaan secara efektif, efisien dan optimal, selalu berfikir, bersikap dan bertindak hati-hati.</p>
@@ -57,7 +57,7 @@
           </div>
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5156/5156874.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/success.png') }}" width="80">
               <div class="text-start">
                 <h5>Ultimate (Unggul)</h5>
                 <p>Sadar bahwa untuk menjadi yang terbaik dan memberikan yang terbaik, memutuskan untuk melakukan sesuatu melebihi standard dan rata-rata orang lain.</p>
@@ -66,7 +66,7 @@
           </div>
           <div class="col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
             <div class="service-item d-block d-lg-flex text-center text-lg-start align-items-start p-3">
-              <img class="bg-light p-1 mb-2 me-3 rounded-circle" src="https://image.flaticon.com/icons/svg/5156/5156788.svg" width="80">
+              <img class="bg-light p-1 mb-2 me-3" src="{{ asset('assets/images/icon/rating.png') }}" width="80">
               <div class="text-start">
                 <h5>Service Excellent ( Pelayanan yang Bermutu)</h5>
                 <p>Memberikan pelayanan yang bermutu, bekerja dan melayani dengan baik saja tidak cukup, harus bekerja dengan predikat yang luar biasa.</p>
